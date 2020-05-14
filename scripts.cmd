727740c90aadae0293ff8cb8414372d42fb30e15
Emre Çakır <ceamkrier@gmail.com>
2020-05-15 02:32:22 +0300

    refactor: script runner added

--- scripts.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-
-if [%1] == [] ( @echo on && echo Please specify your command. && @echo off  )
-if [%1] == [run] ( @deno.exe "run" "--allow-net" "--allow-read" "index.ts" ) else ^
-if [%1] == [clean-run] ( @deno.exe "run" "--allow-net" "--allow-read" "--reload" "index.ts" ) ^
-else ( @echo on && echo Supplied arguments did not match with available ones. )
